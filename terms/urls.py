65b99cae24230c95895d06c2e6d12266c34a7c41
Abdallah Dheir <tareqm577@gmail.com>
2023-02-14 16:38:33 +0200

    add load more and faux pagination

--- terms/urls.py
+++ terms/urls.py
@@ -7,5 +7,17 @@ urlpatterns = [
     path('terms', views.KeywordsListView.as_view(), name='keyword-by-page'),
     path('terms/<int:page>', views.KeywordsListView.as_view(),
          name='keyword-by-page'),
-    path('keyword.json', views.listing_api, name="keyword-api")
+    path('keyword.json', views.listing_api, name="keyword-api"),
+    path(
+        "faux",
+        views.AllKeywordsListView.as_view(
+            template_name="terms/faux_pagination.html"
+        ),
+    ),
+    path(
+        "load_more",
+        views.AllKeywordsListView.as_view(
+            template_name="terms/load_more.html"
+        ),
+    ),
 ]
